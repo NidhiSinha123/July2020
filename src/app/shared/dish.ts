6993097647109e24bdc67ed054738e6e02dcad41
Nidhi <nidhi4truth@gmail.com>
2020-07-17 14:45:06 +0530

    Menu component created.

---
+++ src/app/shared/dish.ts
@@ -0,0 +1,13 @@
+export class Dish
+{
+    id:String;
+    name:String;
+    image:String;
+    category:string;
+    featured:boolean;
+    label:string;
+    price:string;
+    description:string;
+
+
+}
