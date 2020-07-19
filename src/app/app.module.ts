c5a9fd576fc49eae0e5fb0174caaa9d037ad08dc
Nidhi <nidhi4truth@gmail.com>
2020-07-19 11:00:21 +0530

    Added RouterModule into the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,26 +5,35 @@ import { AppComponent } from './app.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import 'hammerjs';
-import { MenuComponent } from './menu/menu.component';
 import {MatListModule} from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
-import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { DishService } from './services/dish.service';
+
+import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactsComponent } from './contacts/contacts.component';
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactsComponent
   ],
   imports: [
       BrowserModule,BrowserAnimationsModule,MatToolbarModule,FlexLayoutModule
-      ,MatListModule,MatGridListModule,MatCardModule,MatButtonModule
+      ,MatListModule,MatGridListModule,MatCardModule,MatButtonModule,AppRoutingModule
   ],
   providers: [
     DishService
